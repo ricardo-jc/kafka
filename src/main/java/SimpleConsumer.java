b9f83e63a63713e816721e853b7d11a510eba92d
ricardo-jc <renjucai123@163.com>
2019-07-16 16:37:21 +0800

    demo version

--- src/main/java/SimpleConsumer.java
+++ src/main/java/SimpleConsumer.java
@@ -1,16 +1,35 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 public class SimpleConsumer {
-    private final ConsumerConnector consumer;
-    private final String topic;
+    private  final KafkaConsumer<String, String> consumer;
 
-    public SimpleConsumer(String zookeeper, String groupId, String topic) {
-        Properties properties = new Properties();
-        //定义连接zookeeper信息
-        properties.put("zookeeper.connect", zookeeper);
-        //定义Consumer所有的groupId
-        properties.put("group.id", groupId);
-        properties.put("zookeeper.session.timeout.ms", "500");
-        
+    private SimpleConsumer(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumer = new KafkaConsumer<>(props);
+    }
+
+    private void consume(){
+        consumer.subscribe(Collections.singletonList(SimpleProducer.TOPIC));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ZERO);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+
+    public  static  void main(String[] args){
+        new SimpleConsumer().consume();
     }
 }
--- src/main/java/SimpleProducer.java
+++ src/main/java/SimpleProducer.java
@@ -1,25 +1,50 @@
+import org.apache.kafka.clients.producer.*;
+
 import java.util.Properties;
+import java.util.Scanner;
 
 public class SimpleProducer {
-    private static Producer<Integer, String> producer;
-    private final Properties properties = new Properties();
-    public SimpleProducer() {
-        //定义连接的broker list
-        properties.put("metadata.broker.list", "192.168.1.216:9092");
-        //定义序列化类，Java中对象传输前要序列化
-        properties.put("serializer.class","kafka.serializer.StringEncoder");
-        producer = new Producer<Integer, String>(new ProducerConfig(properties));
+    private final Producer<String, String> kafkaProducer;
+    private static int key = 0;
+    public final static String TOPIC = "JAVA_TOPIC";
+
+    private SimpleProducer() {
+        kafkaProducer = createKafkaProducer();
     }
+
+    private Producer<String, String> createKafkaProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return new KafkaProducer<>(props);
+    }
+
+    private void produce() {
+        while (true) {
+            System.out.print("输入消息:");
+            Scanner scanner = new Scanner(System.in);
+            String data = scanner.next();
+            if(data.equals("^")) break;
+            kafkaProducer.send(new ProducerRecord<>(TOPIC, String.valueOf(++key), data), (x, y) -> {
+                if(x != null) {
+                    System.out.println("message send success.");
+                }
+                if(y != null) {
+                    System.out.println("message send failed.");
+                    y.printStackTrace();
+                }
+            });
+        }
+    }
+
     public static void main(String[] args) {
-        SimpleProducer sp = new SimpleProducer();
-        //定义topic
-        String topic = "myTopic";
-        //定义发给topic的信息
-        String messageStr = "This is a message."；
-        //构建消息对象
-        KeyedMessage<Integer, String> data = new KeyedMessage<Integer, String>(topic, messageStr);
-        //推送到broker
-        producer.send(data);
-        producer.close();
+        new SimpleProducer().produce();
     }
 }
